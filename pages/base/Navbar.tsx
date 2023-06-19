86736997caab67c6238c37d54e35b46b78571b0e
nahida akter <tanjila@kaz.com.bd>
2023-06-19 19:13:20 +0600

    navbar design

--- pages/base/Navbar.tsx
+++ pages/base/Navbar.tsx
@@ -2,113 +2,199 @@ import { useState } from "react";
 import StoreDialogue from "./StoreDialogue";
 
 const Navbar = () => {
+  const [isStoreDialogueOpen, setIsStoreDialogueOpen] = useState(false);
 
-    const [isStoreDialogueOpen, setIsStoreDialogueOpen] = useState(false);
+  const handleStoreOpen = () => {
+    setIsStoreDialogueOpen(!isStoreDialogueOpen);
+  };
 
-    const handleStoreOpen = () => {
-        setIsStoreDialogueOpen(!isStoreDialogueOpen);
-    };
+  return (
+    <div className="nav-container">
+      <nav>
+        <img className="logo" src="../logo/logo (1) 1.png" alt="Logo" />
 
-
-    return (
-        <nav>
-            <div className="logo">
-                <img src="../logo/logo (1) 1.png" alt="Logo" />
+        <div className="home-container">
+          <div className="home">Home</div>
+        </div>
+        <div className="shop-container" onClick={handleStoreOpen}>
+          <div className="shop">Shop</div>
+        </div>
+        <div className="blog-container">
+          <div className="blog">Blogs</div>
+        </div>
+        <div className="showrooms-container">
+          <div className="showrooms">Showrooms</div>
+        </div>
+        <div className="help-container">
+          <div className="help">Help</div>
+        </div>
+        {/* <div className="signin-button">
+        <button>Sign In</button>
+      </div> */}
+        <div className="buttons-container">
+          <div className="search-container">
+            <div className="search-box">
+              <img src="../logo/search.svg" alt="here" />
             </div>
-            <div className="home-container">
-                <p className="home">Home</p>
+          </div>
+          <div className="profile-container">
+            <div className="profile-box">
+              <img src="../logo/user.svg" alt="here" />
             </div>
-            <div className="shop-container" onClick={handleStoreOpen}>
-                <p className="shop">Shop</p>
+          </div>
+          <div className="cart-container">
+            <div className="cart-box">
+              <img src="../logo/shopping-cart.svg" alt="here" />
             </div>
-            <div className="blog-container">
-                <p className="blog">Blog</p>
-            </div>
-            <div className="signin-button">
-                <button>Sign In</button>
-            </div>
+          </div>
+        </div>
 
+        {isStoreDialogueOpen && (
+          <div className="dialogue-overlay">
+            <StoreDialogue />
+          </div>
+        )}
 
-            {isStoreDialogueOpen && (
-                <div className="dialogue-overlay">
-                    <StoreDialogue />
-                </div>
-            )}
+        <style jsx>
+          {`
+            nav {
+              display: flex;
+              // justify-content: space-between;
+              // align-items: center;
+              // padding: 1rem;
+              background-color: #000;
+              position: sticky;
+              top: 0;
+              left: 0;
+              width: 1920px;
+              height: 78px;
+              overflow: hidden;
+              font-size: var(--font-size-lg);
+            }
 
+            .logo {
+              position: absolute;
+              top: 27px;
+              left: 23px;
+              width: 103.81px;
+              height: 32px;
+              object-fit: cover;
+            }
 
-            <style jsx>
-                {`
-          nav {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 1rem;
-            background-color: #f5f5f5;
-          }
+            .title {
+              flex-grow: 1;
+              text-align: center;
+              font-size: 1.2rem;
+              font-weight: bold;
+            }
+            .home-container {
+              position: absolute;
+              top: 28px;
+              left: 650px;
+              width: 89px;
+              height: 34px;
+            }
+            .shop-container {
+              position: absolute;
+              top: 28px;
+              left: 771px;
+              width: 89px;
+              height: 34px;
+            }
+            .help {
+              position: absolute;
+              width: 41px;
+              height: 27px;
+              left: 1205px;
+              top: 31px;
 
-          .logo img {
-            position: absolute;
-            width: 103.81px;
-            height: 32px;
-            left: 23px;
-            top: 27px;
-          }
+              font-family: "Poppins";
+              font-style: normal;
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 27px;
+            }
+            .showrooms {
+              position: absolute;
+              width: 108px;
+              height: 27px;
+              left: 1027px;
+              top: 31px;
 
-          .title {
-            flex-grow: 1;
-            text-align: center;
-            font-size: 1.2rem;
-            font-weight: bold;
-          }
-          .home-container {
-            position: absolute;
-            width: 54px;
-            height: 31px;
-            left: 668px;
-            top: 31px;
-          }
-          .home {
-            position: absolute;
-            width: 54px;
-            height: 27px;
-            left: 668px;
-            top: 31px;
+              font-family: "Poppins";
+              font-style: normal;
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 27px;
+            }
+            .blog {
+              position: absolute;
+              width: 55px;
+              height: 30px;
+              left: 909px;
+              top: 30px;
 
-            font-family: "Poppins";
-            font-style: normal;
-            font-weight: 500;
-            font-size: 18px;
-            line-height: 27px;
+              font-family: "Poppins";
+              font-style: normal;
+              font-weight: 500;
+              font-size: 18px;
+              line-height: 30px;
+            }
+            .shop {
+              position: absolute;
+              top: 3px;
+              left: 21px;
+              font-weight: 500;
+              color: white;
+            }
+            .home {
+              // position: absolute;
+              top: 3px;
+              left: 18px;
+              font-weight: 500;
+              color: white;
+            }
 
-            color: #000;
-          }
+            .buttons-container {
+              display: flex;
+              flex-direction: row;
+              align-items: flex-start;
+              padding: 0px;
+              gap: 16px;
 
-          .signin-button button {
-            background-color: orange;
-            color: white;
-            padding: 0.5rem 1rem;
-            border: none;
-            border-radius: 4px;
-            font-size: 1rem;
-            cursor: pointer;
-          }
+              position: absolute;
+              width: 134px;
+              height: 34px;
+              left: 1763px;
+              top: 25px;
+            }
 
-          .dialogue-overlay {
-            position: fixed;
-            top: 100px; /* Adjust the top value to align the dialogue as desired */
-            left: 50%;
-            transform: translateX(-50%);
-            z-index: 10; /* Higher z-index value to appear above other content */
-            background-color: #ffffff;
-            border: 1px solid #ccc;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 1rem;
-          }
+            .signin-button button {
+              background-color: orange;
+              color: white;
+              padding: 0.5rem 1rem;
+              border: none;
+              border-radius: 4px;
+              font-size: 1rem;
+              cursor: pointer;
+            }
 
-        `}
-            </style>
-        </nav>
-    );
+            .dialogue-overlay {
+              position: fixed;
+              top: 100px; /* Adjust the top value to align the dialogue as desired */
+              left: 50%;
+              transform: translateX(-50%);
+              z-index: 10; /* Higher z-index value to appear above other content */
+              background-color: #ffffff;
+              border: 1px solid #ccc;
+              box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+              padding: 1rem;
+            }
+          `}
+        </style>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,91 @@
-import HomeTop from './base/HomeTop';
-import GetProducts from './catalog/product/productList/GetProducts';
+import HomeTop from "./base/HomeTop";
+import GetProducts from "./catalog/product/productList/GetProducts";
 
 const HomePage = () => {
   return (
     <>
       <div className="homepage-container">
-        <HomeTop />
-        <GetProducts />
+        {/* <HomeTop />
+        <GetProducts /> */}
+        <img className="red-sofa-bg" alt="" src="../logo/red-sofa-bg.png" />
+        <div className="home-child"></div>
+        <div className="furnishing-crafts-parent">
+          <b className="furnishing-crafts">Furnishing crafts</b>
+          <div className="the-expression-of">The expression of a house</div>
+          <img className="group-child" alt="" src="../logo/vector-2.svg" />
+
+          <img className="group-item" alt="" src="../logo/vector-2.svg" />
+        </div>
       </div>
 
       <style jsx>{`
-                .homepage-container {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                }
-                .one{
-                  font-size: 500px;
-                  color: brown;
-                }
-            `}</style>
+        .homepage-container {
+          // display: flex;
+          // flex-direction: column;
+          // align-items: center;
+          position: relative;
+          background-color: var(--white);
+          width: 100%;
+          height: 10799px;
+          overflow: hidden;
+          text-align: left;
+          font-size: var(--text-btn-size);
+          color: var(--white);
+          font-family: var(--text-btn);
+        }
+        .furnishing-crafts-parent {
+          position: absolute;
+          top: 197px;
+          left: calc(50% - 525px);
+          width: 1117px;
+          height: 257px;
+          font-size: var(--font-size-61xl);
+        }
+        .furnishing-crafts {
+          position: absolute;
+          top: 0;
+          left: calc(50% - 558.5px);
+        }
+        .the-expression-of {
+          position: absolute;
+          top: 159px;
+          left: calc(50% - 260.5px);
+          font-size: var(--font-size-46xl);
+          font-weight: 100;
+        }
+        .group-child,
+        .group-item {
+          position: absolute;
+          top: 67.5px;
+          left: 739px;
+          width: 252px;
+          height: 1px;
+        }
+        .group-item {
+          top: 207.5px;
+          left: 0;
+        }
+        .home-child {
+          position: absolute;
+          top: 0;
+          left: 0;
+          background-color: var(--color-gray-100);
+          width: 1920px;
+          height: 1080px;
+        }
+        .red-sofa-bg {
+          position: absolute;
+          top: 0;
+          left: calc(50% - 960px);
+          width: 1920px;
+          height: 1108px;
+          object-fit: cover;
+        }
+        .one {
+          font-size: 500px;
+          color: brown;
+        }
+      `}</style>
     </>
   );
 };
